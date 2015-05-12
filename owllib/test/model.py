830e5f2ea9a542117ad70ef1c2163ded7882717b
Zhe Liu <luciferz2012@gmail.com>
2015-05-12 17:44:55 +0800

    implement directImports and imports

--- owllib/test/model.py
+++ owllib/test/model.py
@@ -3,6 +3,7 @@ from rdflib import Graph
 from rdflib.term import BNode
 from rdflib import RDF, OWL
 from rdflib.util import guess_format
+from rdflib.plugin import PluginException
 
 
 class OWLEntity:
@@ -99,24 +100,58 @@ class OWLObjectProperty(OWLProperty):
 
 
 class OWLOntology(OWLEntity):
+	_guessFormats = set(['n3'])  # TODO
+	_IRIDict = {}
+
+	def __new__(cls, IRI=None, versionIRI=None):
+		self = super(OWLOntology, cls).__new__(cls)
+		if IRI:
+			ontDict = OWLOntology._IRIDict.get(IRI, {})
+			OWLOntology._IRIDict[IRI] = ontDict
+			self = ontDict.get(versionIRI, self)
+			ontDict[versionIRI] = self
+		return self
+
 	def __init__(self, IRI=None, versionIRI=None):
 		super(OWLOntology, self).__init__(IRI)
-		self.graph = Graph()
-		self.graph.add((self._IRI, RDF.type, OWL.Ontology))
-		if versionIRI:
-			self.graph.add((IRI, OWL.versionIRI, versionIRI))
-		self._directImports = None
+		if not hasattr(self, 'graph'):
+			self.graph = Graph()
+			self.graph.add((self.IRI, RDF.type, OWL.Ontology))
+			if versionIRI:
+				self.graph.add((self.IRI, OWL.versionIRI, versionIRI))
+			self._directImports = None
 
-	def load(self, file=None, location=None, data=None, owlFormat=None):
+	@staticmethod
+	def _load(file=None, location=None, data=None, owlFormat=None):
 		if location and not owlFormat:
 			owlFormat = guess_format(location)
-		self.graph.parse(format=owlFormat, file=file, location=location, data=data)
+		graph = Graph()
+		try:
+			return graph.parse(file=file, location=location, data=data, format=owlFormat)
+		except PluginException as e:
+			for f in OWLOntology._guessFormats:
+				try:
+					return graph.parse(file=file, location=location, data=data, format=f)
+				except PluginException:
+					pass
+			raise e
+
+	@staticmethod
+	def load(file=None, location=None, data=None, owlFormat=None):
+		graph = OWLOntology._load(file, location, data, owlFormat)
 		maxNumOfTriples = 0
-		for IRI in self.graph.subjects(RDF.type, OWL.Ontology):
-			triples = [(p, o) for (p, o) in self.graph.predicate_objects(IRI)]
+		maxIRI = None
+		for IRI in graph.subjects(RDF.type, OWL.Ontology):
+			triples = [(p, o) for (p, o) in graph.predicate_objects(IRI)]
 			if maxNumOfTriples < len(triples):
 				maxNumOfTriples = len(triples)
-				self._IRI = IRI
+				maxIRI = IRI
+		if maxIRI in OWLOntology._IRIDict:
+			return OWLOntology(maxIRI)
+		else:
+			ont = OWLOntology(maxIRI)
+			ont.graph += graph
+			return ont
 
 	def save(self):
 		raise NotImplementedError
@@ -126,18 +161,16 @@ class OWLOntology(OWLEntity):
 		if not self._directImports:
 			self._directImports = set()
 			for IRI in self.graph.objects(self.IRI, OWL.imports):
-				ont = OWLOntology()
-				ont.load(location=IRI)
-				self._directImports.add(ont)
+				self._directImports.add(OWLOntology.load(location=IRI))
 		return self._directImports
 
 	@property
 	def imports(self):
-		raise NotImplementedError
-
-	@property
-	def ontologyManager(self):
-		raise NotImplementedError
+		imps = set()
+		for imp in self.directImports:
+			imps.add(imp)
+			imps |= imp.directImports
+		return imps
 
 	@property
 	def allEntities(self):
--- owllib/test/test.py
+++ owllib/test/test.py
@@ -1,24 +1,42 @@
 from unittest import TestCase, main
 from codecs import open
 
+from rdflib.term import URIRef
+
 from owllib.test.model import OWLOntology
 
 
 class TestOntology(TestCase):
+	def test_new(self):
+		IRI = URIRef('a')
+		versionIRI = URIRef('b')
+		ont1 = OWLOntology(IRI)
+		ont2 = OWLOntology(IRI)
+		ont3 = OWLOntology(IRI, versionIRI)
+		self.assertEqual(ont1, ont2)
+		self.assertNotEqual(ont1, ont3)
+
 	def test_init(self):
 		OWLOntology()
 
-	def test_load(self):
-		ont = OWLOntology()
-		f = open('../ont/test.n3', encoding='utf-8')
-		ont.load(file=f, owlFormat='n3')
+	def test_load(self, location='../ont/test.n3'):
+		f = open(location, encoding='utf-8')
+		ont = OWLOntology.load(file=f, owlFormat='n3')
 		f.close()
 		return ont
 
+	def test_load_same_IRI(self):
+		ont1 = OWLOntology.load(location='http://eulersharp.sourceforge.net/2003/03swap/workflow')
+		ont2 = OWLOntology.load(location='http://eulersharp.sourceforge.net/2003/03swap/workflow')
+		self.assertEqual(ont1, ont2)
+
 	def test_directImports(self):
-		ont = self.test_load()
-		for imp in ont.directImports:
-			print(imp)
+		ont = self.test_load('../ont/import.n3')
+		self.assertEqual(len(ont.directImports), 2)
+
+	def test_imports(self):
+		ont = self.test_load('../ont/import.n3')
+		self.assertEqual(len(ont.imports), 2)
 
 
 if __name__ == '__main__':
